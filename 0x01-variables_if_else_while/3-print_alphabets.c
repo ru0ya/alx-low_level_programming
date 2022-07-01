b12720957b21067dcb28ad99250a5722e023b702
ru0ya <viktaangi02@gmail.com>
2022-07-01 13:41:58 -0700

    printa

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,10 +6,13 @@
 
 int main(void)
 {
-	int ch;
-	
-	printf("Print all uppercase letters followed by all lower case letters on the next line :\n");
-	for (ch= 'A';ch<= 'Z' ; ch++)
-		putchar(ch);
-		put
+	char alphabet;
+	/* Program */
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
 
