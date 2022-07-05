55701d381bd60aa3a1b44dd1d5a045bc47790953
ru0ya <viktaangi02@gmail.com>
2022-07-05 12:17:30 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include"main.h"
 /**
  * main-prints putchar
@@ -11,12 +10,12 @@ int main(void)
 	_putchar('_');
 	_putchar('p')
 	_putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
