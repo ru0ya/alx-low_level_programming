f5539fba02de070c2ef852662fcf2019e5ec9e0c
ru0ya <viktaangi02@gmail.com>
2022-07-05 05:58:17 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,31 @@
 #include<stdio.h>
 /**
- * main- Entry point
+ * main-Entry point
  * Return:0 (success)
 */
 int main(void)
-{
-	printf("_putchar"\n);
 
+{
+	char a, b, c, d, e, f, g, h, i, j;
+
+	a = ' _ ';
+	b = ' p ';
+	c = ' u ';
+	d = ' t ';
+	e = ' c ';
+	f = ' h ';
+	g = ' a ';
+	h = ' r ';
+	i = '\n';
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(d);
+	putchar(e);
+	putchar(f);
+	putchar(g);
+	putchar(h);
+	putchar(i);
 	return (0);
 }
 
