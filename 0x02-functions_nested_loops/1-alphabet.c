61e6052ce1585161712670644f245625c21fa4ee
ru0ya <viktaangi02@gmail.com>
2022-07-05 04:56:51 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * print_alphabet - print the alphabet in lowercase
+ * followed by a new line
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+}
+/**
+ * main - Entry point
+ * Return:0 (success)
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
