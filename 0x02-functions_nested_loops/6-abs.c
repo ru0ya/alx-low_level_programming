740bdd31e5302f353a5da6c3250c163351a075b3
ru0ya <viktaangi02@gmail.com>
2022-07-06 00:56:11 -0700

    main.h

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * _abs - compute absolute value of a number
+ * @k: integer
+ *
+ * Return: Always 0 (success)
+*/
+
+int _abs(int k)
+{
+	if (k >= 0)
+		return (k);
+	k *= -1;
+	return (k);
+}
