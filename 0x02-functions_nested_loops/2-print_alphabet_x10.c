d50d7cd0d2d3b6e231052500d34965a4e94adbb2
ru0ya <viktaangi02@gmail.com>
2022-07-06 00:02:56 -0700

    alphx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void);
 	char alphabet;
 	int i;
 
-	for (i = 0; i<10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			_putchar(alphabet);
