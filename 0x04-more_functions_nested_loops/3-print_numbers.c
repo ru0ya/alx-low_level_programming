5fec7bac0bc359e9de640bf0ac54efd0dd0fd257
ru0ya <viktaangi02@gmail.com>
2022-07-07 07:29:50 -0700

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_numbers - prints characters
+ * Return: 0 (success)
+ */
+
+void print_numbers(void);
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		_putchar(n++);
+	}
+	_putchar (10);
+}
