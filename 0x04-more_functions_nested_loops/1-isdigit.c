cd7bc99823c21d1cb4c4b95249e5bd1f7dd25033
ru0ya <viktaangi02@gmail.com>
2022-07-07 06:18:02 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ *  _isdigit - Check for uppercase characters
+ * @c: integer
+ * Return: 1 (success) 0 (otherwise)
+*/
+
+int _isdigit(int c)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (c == i)
+			return (1);
+	}
+	return (0);
+}
+
