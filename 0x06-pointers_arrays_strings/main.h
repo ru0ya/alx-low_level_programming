90258a8fe232d7fd5af92ac140ec7cb1eddecba2
ru0ya <viktaangi02@gmail.com>
2022-07-13 04:28:33 -0700

    main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcat - concatenate two strings
+ *
+ * Return: char *
+ */
+
+char *_strcat(char *dest, char *src);
+
+#endif /*MAIN_H*/
