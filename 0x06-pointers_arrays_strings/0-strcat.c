6befd397b0748b19e22de46702d92313682551e6
ru0ya <viktaangi02@gmail.com>
2022-07-13 05:27:59 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/*
+ * _strcat - concatenate two strings
+ * @i: integer
+ * @k: integer
+ *
+ * Return: destination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int k = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[k] != '\0')
+	{
+		dest[i++] = src[k++];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
