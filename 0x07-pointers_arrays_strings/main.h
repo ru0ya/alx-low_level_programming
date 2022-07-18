b51edf65b2963b29c803a9639946b6cd9d025bbc
ru0ya <viktaangi02@gmail.com>
2022-07-18 18:03:26 +0300

    task6

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,5 +13,24 @@ char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
 
+void print_chessboard(char (*a)[8]);
+
+/**
+ * print_diagsums - print sums of diagonals in matrix
+ * @a: matrix
+ * @size: size of matrix
+ */
+
+void print_diagsums(int *a, int size);
+
+/**
+ * set_string - set value of pointer to a char
+ * @s: variable of type pointer
+ * @to: char
+ */
+
+void set_string(char **s, char *to);
+
+
 
 #endif /*MAIN_H*/
