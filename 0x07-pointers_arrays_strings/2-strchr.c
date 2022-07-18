0d56bb02fdd2fd281ed469d8ee8cea64c11e394d
ru0ya <viktaangi02@gmail.com>
2022-07-18 22:41:06 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,10 +10,13 @@
 
 char *_strchr(char *s, char c)
 {
-	f0r (; *s != '\0'; s++)
-	{
-		if (*s == c)
-			return (char *) s;
-	}
-	return (NULL);
+	int i = 0;
+
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return (0);
 }
