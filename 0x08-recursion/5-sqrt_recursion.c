e02c5597c5f5a4c4ad4413f37e442db67a002abc
ru0ya <viktaangi02@gmail.com>
2022-07-20 17:17:14 +0300

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _sqrt_recursion - outputs squareroot of n
+ *
+ * @n: value
+ * Return: void
+ */
+
+int _sqrt_recursion(int n);
+{
+	if (n == 0)
+	{
+		return (-1);
+	}
+	else
+		return (sqrt(n));
+}
