3f7f0e19d477957e981d2115b0cb55b21777a6fc
ru0ya <viktaangi02@gmail.com>
2022-07-20 16:27:15 +0300

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * _strlen - outputs the length of a string
+ *
+ * @s: string
+ *
+ * Return: void
+ */
+
+int _strlen_recursion(char *s);
+{
+	_putchar ("len: %lu\n", strlen(s));
+
+	return (0);
+}
