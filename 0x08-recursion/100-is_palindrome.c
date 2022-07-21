90de84d6cd62891532905ab672045b67f66e0be0
ru0ya <viktaangi02@gmail.com>
2022-07-21 09:35:23 +0300

    is_palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ * is_palindrome - checks whether a string in reverse
+ * is same as in normal state
+ *
+ * startindex: beginning of string
+ * endindex: end of string
+ *@s: string
+ * Return: void
+ */
+
+int is_palindrome(char *s)
+{
+	int startIndex;
+	int endIndex;
+
+	if (s[startIndex] != s[endIndex])
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+
+	return (is_palindrome(s));
+}
