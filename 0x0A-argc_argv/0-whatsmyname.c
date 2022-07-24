a253fee250db4c6d0f62d4b95428ea1895124575
ru0ya <viktaangi02@gmail.com>
2022-07-24 17:19:51 +0300

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,20 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int count;
+	printf("%s", argv[0]);
 
-	printf("my name is\"%s\" .\n", argv[0]);
-
-	if (argc > 1)
-	{
-		for (count = 1; count < argc; count++)
-		{
-			printf("argv[%d] = %s\n", count, argv[count]);
-		}
-	}
-	else
-	{
-		printf("my new name is\n");
-	}
+	return (0);
 }
-
