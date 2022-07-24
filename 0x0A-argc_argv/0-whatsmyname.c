6fea435cf459d0f3c1dad8e29ac0ed4a07f79fc4
ru0ya <viktaangi02@gmail.com>
2022-07-24 16:57:38 +0300

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * @main: parameter 
+ * @argc: first command line argument
+ * @argv: points to last argument
+ *@count: number of arguments
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	printf("my name is\"%s\" .\n", argv[0]);
+
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			printf("argv[%d] = %s\n", count, argv[count]);
+		}
+	}
+	else
+	{
+		printf("my new name is\n");
+	}
+}
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stddef.h>
+
+int main(int argc, char *argv[]);
+
+
+#endif /*MAIN_H*/
