73acfd11fae0df5c890aa1aff7956d794a8e21f3
ru0ya <viktaangi02@gmail.com>
2022-07-25 11:55:57 +0300

    mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of arguments
+ * @argv: name of arguments
+ *
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+
+	if (argc != 3)
+	{
+
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+}
+
+
