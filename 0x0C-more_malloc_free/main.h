57575962fd528143aa0330a4ccacc8121de91c41
ru0ya <viktaangi02@gmail.com>
2022-07-28 21:58:38 +0300

    calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -17,7 +17,7 @@ int _putchar(char c);
 
 void *malloc_checked(unsigned int b);
 
-char *string_nconcat(char *s1, char *s2, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif /*MAIN_H*/
 
