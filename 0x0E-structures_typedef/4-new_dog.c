e4db65f7272189c26c57c707a9d340146c36e9cd
ru0ya <viktaangi02@gmail.com>
2022-08-02 23:07:57 +0300

    store a copy

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,16 +15,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	namelen = strlen(name) +1;
-	ownlen = strlen(owner) +1;
+	namelen = strlen(name) + 1;
+	ownlen = strlen(owner) + 1;
 
 	r->name = malloc(sizeof(r->name) * namelen);
 	if (r->name == NULL)
 	{
-		free(r);
-		return(NULL);
+		free (r);
+		return (NULL);
 	}
-	for (i = 0; i < namelen -1; i++)
+	for (i = 0; i < namelen - 1; i++)
 	{
 		r->name[i] = name[i];
 		 printf("name: %c %c\n", r->name[i], name[i]);
@@ -32,7 +32,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	r->age = age;
 
-	r->owner = mmalloc(sizeof(r->owner) * ownlen);
+	r->owner = malloc(sizeof(r->owner) * ownlen);
 	if (r->owner == NULL)
 	{
 		free(r->name);
@@ -40,17 +40,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	for (i = 0; i < ownlen; i++)
+	{
 		r->owner[i] = owner[i];
 	printf("owner: %c %c\n", r->owner[i], owner[i]);
 	}
+
 	return (r);
 
-}
-int main(void)
-{
-    dog_t *my_dog;
-
-    my_dog = new_dog("Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
-    return (0);
 }
