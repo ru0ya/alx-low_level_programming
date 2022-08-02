c0e45bfb8b23db91926809148bcc6ae8b7979bd3
ru0ya <viktaangi02@gmail.com>
2022-08-02 13:57:16 +0300

    typrdef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,15 +12,18 @@
  * @owner: third parameter
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
+
+
+
 
 
 #endif /*DOG_H*/
