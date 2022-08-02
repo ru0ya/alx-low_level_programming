5a849730b0a5c5bf861b3525bb83356033d6faed
ru0ya <viktaangi02@gmail.com>
2022-08-02 12:20:46 +0300

    init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 
 /**
  * init_dog - initializes dog
- *
+ *@d: argument
  * @name: character parameter
  * @age: integer parameter
  * @owner: character parameter
@@ -15,15 +15,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		return;
 	}
 
-	d -> name = name;
-	d -> age = age;
-	d -> owner;
-}
-int main(void)
-{
-    struct dog my_dog;
-
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	d->name = name;
+	d->age = age;
+	d->owner;
 }
