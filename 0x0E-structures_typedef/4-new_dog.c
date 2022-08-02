a34bfed37f25493329b901c3a1208b73f3f45450
ru0ya <viktaangi02@gmail.com>
2022-08-02 17:42:26 +0300

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,21 +1,56 @@
 #include"dog.h"
+
 /**
  * 
  *
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *r;
+	int namelen, ownlen, i;
 
-	new_dog = malloc(sizeof(dog_t));
+	r = malloc(sizeof(dog_t));
 
-	if (new_dog == NULL)
+	if (r == NULL)
 	{
 		return (NULL);
 	}
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
+	namelen = strlen(name) +1;
+	ownlen = strlen(owner) +1;
 
-	return (new_dog);
+	r->name = malloc(sizeof(r->name) * namelen);
+	if (r->name == NULL)
+	{
+		free(r);
+		return(NULL);
+	}
+	for (i = 0; i < namelen -1; i++)
+	{
+		r->name[i] = name[i];
+		 printf("name: %c %c\n", r->name[i], name[i]);
+
+	}
+	r->age = age;
+
+	r->owner = mmalloc(sizeof(r->owner) * ownlen);
+	if (r->owner == NULL)
+	{
+		free(r->name);
+		free(r);
+		return (NULL);
+	}
+	for (i = 0; i < ownlen; i++)
+		r->owner[i] = owner[i];
+	printf("owner: %c %c\n", r->owner[i], owner[i]);
+	}
+	return (r);
+
+}
+int main(void)
+{
+    dog_t *my_dog;
+
+    my_dog = new_dog("Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+    return (0);
 }
