212667d7420ae4e3b369122592dda006837613a8
ru0ya <viktaangi02@gmail.com>
2022-08-02 11:10:01 +0300

    errors fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+
 #include<stdio.h>
 #include<stdlib.h>
 
 /**
  * struct dog - entry point
  *
- * @name: parameter of character value
- * @age: parameter of integer value
- * @owner: parameter of character value
+ * @name: first parameter
+ * @age: second parameter
+ * @owner: third parameter
  */
 
 struct dog
@@ -15,18 +18,6 @@ struct dog
 	float age;
 	char *owner;
 };
-struct dog *my_dog(char *name, float age, char *owner)
-{
-	struct dog *my_dog;
 
-	my_dog = malloc(sizeof(struct dog));
-	if (my_dog == NULL)
-	{
-		return (NULL);
-	}
-	my_dog -> name = name;
-	my_dog -> age = age;
-	my_dog -> owner = owner;
 
-	return (my_dog);
-}
+#endif /*DOG_H*/
