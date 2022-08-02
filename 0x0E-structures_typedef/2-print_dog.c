25cc95b64156af352dd4171003a27d0e2091e0ae
ru0ya <viktaangi02@gmail.com>
2022-08-02 12:55:38 +0300

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,26 +8,14 @@
 
 void print_dog(struct dog *d)
 {
-	struct dog *d;
 
-	if (d == NULL)
+
+	if (d != NULL)
 	{
-		return;
-	}
 	printf("name %s\n", d->name);
 	printf("owner %s\n", d->owner);
-	printf("Age %d\n", d->age);
+	printf("Age %f\n", d->age);
+	}
 	
-	return (0);
-}
-int main(void)
-{
-    struct dog my_dog;
-
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    print_dog(&my_dog);
-    return (0);
-}
-	
+	return;
+}	
