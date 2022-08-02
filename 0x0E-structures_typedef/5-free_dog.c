2cc985069a80594c195bb0d2ccf8045d9a6a86bc
ru0ya <viktaangi02@gmail.com>
2022-08-02 23:24:26 +0300

    debugging

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,22 @@
 #include"dog.h"
 #include<stdio.h>
 
+/**
+ * print_dog: frees previously allocated memory
+ *
+ * @d: pointer
+ * Return: void
+ */
 void print_dog(struct dog *d)
 {
 
-	if (d != NULL)
+	if (!d)
 	{
+		return;
+	}
 
 	free((*d).name);
 	free((*d).owner);
 	free(d);
-	}
-	return;
 
 }
