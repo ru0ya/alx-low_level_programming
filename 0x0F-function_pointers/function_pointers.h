ba1df7b7a48d7e0a844eeca5dd16f46b93151091
ru0ya <viktaangi02@gmail.com>
2022-08-03 22:51:37 +0300

    debug3

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
-#ifndef _FUNCTION_POINTERS_H
-#define _FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 
-#include<stdio.h>
+
 #include<stdlib.h>
 
 int _putchar(char c);
