f42a1169701ec1e2ec70edff034bd8f0c9bbb399
ru0ya <viktaangi02@gmail.com>
2022-08-12 22:43:49 +0300

    print lists debugged

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,34 @@
 #include "lists.h"
 
 /**
+ *size_t - prints list
+ *@h: 
  *
- *
- *
+ * Returns: count of list
  */
 
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
+	unsigned int;
 
 	if(h == NULL)
 		return (0);
-
-	h -> next != 0;
-
-	if (h -> str == NULL)
+	while(h)
 	{
-		printf("[%u] and %s\n" h -> len, h -> str;
-				count ++;
+		h -> next != 0;
+		
+		if (!h -> str)
+		{
+		printf("[%d][%s]\n", 0, "(nil)");
+		}
+	else
+		{	
+		printf("[%u][%s]\n", h->len, h->str);
+		count++;
+		h = h->next;
+		}
 	}
+
 	return (count);
-}
+}	
