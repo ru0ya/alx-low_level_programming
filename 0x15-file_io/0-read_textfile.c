81078ec7b84d58e44c6e97358d3e37ebe0b05f78
ru0ya <viktaangi02@gmail.com>
2022-08-22 17:02:46 +0300

    read and print

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t _read, _write;
+	char *buffer = NULL;
+	int fd;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+		if (fd == -1)
+			return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+	_read = read (fd, buffer, letters);
+		if (_read == -1)
+			return (0);
+	_write = write (STDOUT_FILENO, buffer, _read);
+	if (_write == -1)
+		return (0);
+	close (fd);
+	return (_read);
+}
