fb2797f88f28023ef2ebe69a4a33f390935c2859
ru0ya <viktaangi02@gmail.com>
2022-09-14 13:20:17 +0300

    preprocessor tasks in C

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - prints name of file printed 
+ * from
+ *
+ * Return: nothing
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This is a project that focuses on preprocessor in C; preprocessor,compiling,assembly and linking
